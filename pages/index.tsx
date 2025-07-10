623966958e4ef62a4fa7962268b048988f0152ec
terrytaylorbonn <terrytaylorbonn@gmail.com>
2025-07-10 14:23:05 -0400

    Add Algolia verification meta tag GPT25

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
+// pages/index.tsx #426 GPT25
+import Head from 'next/head';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <Layout>
-      <h1>Welcome to My Docs</h1>
-      <p>This is a test site with Algolia DocSearch.</p>
-    </Layout>
+    <>
+      <Head>
+        <meta name="algolia-domain-verification" content="FF14F5E36F82A306" />
+      </Head>
+      <Layout>
+        <h1>Welcome to My Docs</h1>
+        <p>This is a test site with Algolia DocSearch.</p>
+      </Layout>
+    </>
   );
 }
+
+
+// import Layout from '../components/Layout';
+
+// export default function Home() {
+//   return (
+//     <Layout>
+//       <h1>Welcome to My Docs</h1>
+//       <p>This is a test site with Algolia DocSearch.</p>
+//     </Layout>
+//   );
+// }
